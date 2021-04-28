957c15c9d4ec52e184177aa620da2cd6ffdf93ae
Hidetaro7 <hidetaro7@gmail.com>
2021-04-28 18:44:57 +0900

    feat: first commit

---
+++ pages/_slug/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <main class="main">
+    <h1 class="title">{{ sizes }}</h1>
+    <p class="publishedAt">{{ publishedAt }}</p>
+    <div class="post" v-html="summary"></div>
+  </main>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData({ params }) {
+    const { data } = await axios.get(
+      `https://tuqulore.microcms.io/api/v1/instant/${params.slug}`,
+      {
+        headers: { 'X-API-KEY': 'c1567ee1-ca6e-4d8c-b1c7-0263a1130920' }
+      }
+    )
+    console.log(data)
+    return data
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.main {
+  p {
+    @apply text-red-800;
+  }
+}
+</style>
